08be79c394a20ade7d9770388646e4ed9aa319d2
Andrew Dong <andrewdong1994@gmail.com>
2018-07-10 00:28:11 -0400

    Ran into routes error and can't get main to load

---
+++ app/models/user.rb
@@ -0,0 +1,50 @@
+class User < ApplicationRecord
+  after_initialize :ensure_session_token
+
+
+  # VALIDATIONS
+  validates :username, :email, :password_digest, :session_token, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+
+  #ASSOCIATIONS
+
+  #GETTERS/SETTERS
+
+  attr_reader :password
+
+  #METHODS
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return nil unless user
+    user.is_password?(password) ? user : nil
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    return self.session_token
+  end
+
+  def is_password?(password)
+    bcrypted_password = BCrypt::Password.new(self.password_digest)
+    bcrypted_password.is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
+
+end
---
+++ frontend/reducers/users_reducer.js
@@ -0,0 +1,19 @@
+import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
+import { RECEIVE_USER, RECEIVE_USERS } from '../actions/user_actions';
+import { merge } from 'lodash';
+
+export default (state = {}, action) => {
+  Object.freeze(state);
+  const newState = merge({}, state);
+
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+    case RECEIVE_USER:
+      newState[action.user.id] = action.user;
+      return newState;
+    case RECEIVE_USERS:
+      return action.users.users;
+    default:
+      return state;
+  }
+};
