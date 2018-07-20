4f780dff6a92de4d99d977ba61c45f6ef1f7e041
Andrew Dong <andrewdong1994@gmail.com>
2018-07-20 13:13:59 -0400

    final push before presentation - fixed claps sort of

--- app/controllers/api/claps_controller.rb
+++ app/controllers/api/claps_controller.rb
@@ -8,7 +8,7 @@ class Api::ClapsController < ApplicationController
 
     if @clap
 
-      new_clap_count = params[:clap][:clap_count].to_i + @clap.clap_count
+      new_clap_count = params[:clap][:clap_count].to_i + @clap.clap_count + 1
       # new_clap_count = 50 if new_clap_count > 50
 
       if @clap.update(clap_count: new_clap_count)
